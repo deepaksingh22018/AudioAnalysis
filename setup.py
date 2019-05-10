4bb4dda42126d4959dc9ff3b164ba2f570e7805f
deepaksingh22018 <46861364+deepaksingh22018@users.noreply.github.com>
2019-05-11 00:31:02 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyAudioAnalysis',
+      version='0.2.5',
+      description='Python Audio Analysis Library: Feature Extraction, Classification, Segmentation and Applications',
+      url='https://github.com/tyiannak/pyAudioAnalysis',
+      author='Deepak Kumar Singh',
+      author_email='deepaksingh22018@gmail.com
+      license='Apache License, Version 2.0',
+      packages=['pyAudioAnalysis'],
+      zip_safe=False)
